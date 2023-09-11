f4273e86f79d35049b3c9b79e682886dc3d10d46
kundakchan <kundakchan@yandex.ru>
2023-09-11 15:46:24 +0300

    [1] - add eslint

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <button :id="'button-1'" :class="['test', { true: 'test-class' }]">Lorem, ipsum.</button><button>Lorem ipsum dolor sit.</button>
     <NuxtWelcome />
   </div>
 </template>
